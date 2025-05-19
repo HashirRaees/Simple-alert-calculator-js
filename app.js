883d4675eb9f89ce28bc0abfcdb497daf8cd2432
HashirRaees <hashirrais13@gmail.com>
2025-05-19 15:23:04 +0500

    created a calculator using js

---
+++ app.js
@@ -0,0 +1,17 @@
+var fNumber = +prompt("Enter the first number to add:")
+var sNumber = +prompt("Enter the second number to add:")
+alert(fNumber + sNumber)
+
+var fNumber = +prompt("Enter the first number to subtract:")
+var sNumber = +prompt("Enter the second number to subtract:")
+alert(fNumber - sNumber)
+
+var fNumber = +prompt("Enter the first number to multiply:")
+var sNumber = +prompt("Enter the second number to multiply:")
+alert(fNumber * sNumber)
+
+var fNumber = +prompt("Enter the first number to divide:")
+var sNumber = +prompt("Enter the second number to divide:")
+alert(fNumber / sNumber)
+
+alert("Thanks")
